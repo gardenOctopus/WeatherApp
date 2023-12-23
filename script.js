7ebdbeebf2c04635ee8c9109d8ae86a891876692
gardenOctopus <zebraspider@protonmail.com>
2023-12-23 16:41:05 -0500

    Added info from API

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function weather() {
     const location = document.getElementById('location');
     const temperature = document.getElementById('temperature');
     const description = document.getElementById('description');
+    const feelsLike = document.getElementById('feels-like');
+    const wind = document.getElementById('wind');
+    const pressure = document.getElementById('pressure');
+    const humidity = document.getElementById('humidity');
+    const cloud = document.getElementById('cloud');
+    const uv = document.getElementById('uv');
     fetch('https://api.weatherapi.com/v1/current.json?key=875b6fdf98744187b74112956232411&q=london', {mode: 'cors'})
     .then(function(response) {
     return response.json();
@@ -9,7 +15,13 @@ function weather() {
     .then(function(response) {
     location.innerText = response.location.name;
     temperature.innerText = response.current.temp_c;
-    description.innerText = response.condition.text;
+    description.innerText = response.current.condition.text;
+    feelsLike.innerText = response.current.feelslike_c;
+    wind.innerText = response.current.wind_kph;
+    pressure.innerText = response.current.pressure_mb;
+    humidity.innerText = response.current.humidity;
+    cloud.innerText = response.current.cloud;
+    uv.innerText = response.current.uv;
     });
 }
 

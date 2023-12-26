9c2e992db9e29efa86ccad26087127a983c0f575
gardenOctopus <zebraspider@protonmail.com>
2023-12-26 14:06:53 -0500

    Made icon visible

--- script.js
+++ script.js
@@ -8,20 +8,22 @@ function weather() {
     const humidity = document.getElementById('humidity');
     const cloud = document.getElementById('cloud');
     const uv = document.getElementById('uv');
+    const icon = document.getElementById('icon');
     fetch('https://api.weatherapi.com/v1/current.json?key=875b6fdf98744187b74112956232411&q=london', {mode: 'cors'})
     .then(function(response) {
     return response.json();
     })
     .then(function(response) {
     location.innerText = response.location.name;
-    temperature.innerText = response.current.temp_c;
+    temperature.innerText = response.current.temp_c + '°C';
     description.innerText = response.current.condition.text;
-    feelsLike.innerText = response.current.feelslike_c;
-    wind.innerText = response.current.wind_kph;
-    pressure.innerText = response.current.pressure_mb;
-    humidity.innerText = response.current.humidity;
-    cloud.innerText = response.current.cloud;
+    feelsLike.innerText = response.current.feelslike_c + '°C';
+    wind.innerText = response.current.wind_kph + 'km/h';
+    pressure.innerText = response.current.pressure_mb + 'mb';
+    humidity.innerText = response.current.humidity + '%';
+    cloud.innerText = response.current.cloud + '%';
     uv.innerText = response.current.uv;
+    icon.src = 'https:' + response.current.condition.icon;
     });
 }
 
